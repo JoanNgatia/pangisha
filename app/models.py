d0d72d899bda125d5b80d1ba3de9cb988acb68eb
JoanNgatia <joan.ngatia@andela.com>
2017-11-01 16:50:05 +0300

    Add models and tests

--- app/models.py
+++ app/models.py
@@ -3,4 +3,25 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Building(models.Model):
+    """Building details."""
+
+    owner = models.CharField(max_length=255)
+
+
+class Tenant(models.Model):
+
+    name = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=15)
+    national_id = models.CharField(max_length=20)
+
+
+class House(models.Model):
+    building = models.ForeignKey(
+        Building, related_name='houses', on_delete=models.CASCADE)
+    description = models.TextField()
+    number = models.CharField(max_length=3)
+    tenant = models.ForeignKey(
+        Tenant, on_delete=models.SET_NULL, related_name='tenants', null=True)
+    rent_expected = models.CharField(max_length=6)
--- app/tests.py
+++ app/tests.py
@@ -4,3 +4,26 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
+from models import Building, House
+
+
+class ModelTestCase(TestCase):
+    """Define correct model creation."""
+
+    def setUp(self):
+        """Add initial data."""
+        self.owner = 'J'
+        self.new_building = Building(owner=self.owner)
+
+    def test_new_model_creation(self):
+        """Check new model instantiation."""
+        initial_count = Building.objects.count()
+        self.new_building.save()
+        new_count = Building.objects.count()
+        self.assertNotEqual(initial_count, new_count)
+
+        self.number = 'B7'
+        self.new_house = House(
+            number=self.number, building=self.new_building)
+        self.new_house.save()
+        self.assertEqual(len(House.objects.all()), 1)
